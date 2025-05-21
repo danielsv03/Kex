c3a85d8d06894fa9267a88662508ac9ac7131d2d
danielsv03 <daniels5@kth.se>
2025-05-21 14:44:13 +0200

    dsds

--- Simulation/Simulation.py
+++ Simulation/Simulation.py
@@ -11,7 +11,7 @@ import csv
 
 from Car import Car
 # One of: "Zipper", "TimeBased", "PriorityBased", "AuctionBased"
-CURRENT_HEURISTIC = "Zipper"
+CURRENT_HEURISTIC = "TimeBased"
 run_count = 0
 if (len(sys.argv) < 2):
    run_count = 1
